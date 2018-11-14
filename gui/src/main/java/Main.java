d7a21254043d31e46b0ed7fb624336e8fffc2f12
Juho Taakala <juho.taakala@cs.tamk.fi>
2018-11-14 19:45:47 +0200

    gui

--- gui/src/main/java/Main.java
+++ gui/src/main/java/Main.java
@@ -5,6 +5,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -28,30 +29,43 @@ public class Main extends Application {
                             "Item 6:", "Item 7:", "Item 8:", "Item 9:", "Item 10:"};
 
         Label shoppingList = new Label("Shopping List");
+        shoppingList.setFont(new Font("Arial", 20));
         GridPane.setConstraints(shoppingList, 0, 0);
 
         // add all labels
         for (int i = 0; i < items.length; i++) {
             Label itemLabel = new Label(items[i]);
+            itemLabel.setFont(new Font("Arial", 16));
             GridPane.setConstraints(itemLabel, 0, i +1);
             grid.getChildren().add(itemLabel);
         }
 
-        TextField[] textFields = new TextField[10];
+        TextField[] itemField = new TextField[10];
 
-        // add all textfields
+        // add all item textfields
         for (int i = 0; i < 10; i++) {
             TextField textField = new TextField();
             textField.setPromptText("item");
             GridPane.setConstraints(textField, 1, i +1);
-            textFields[i] = textField;
+            itemField[i] = textField;
+            grid.getChildren().add(textField);
+        }
+
+        TextField[] amountField = new TextField[10];
+
+        // add all amount textfields
+        for (int i = 0; i < 10; i++) {
+            TextField textField = new TextField();
+            textField.setPromptText("amount");
+            GridPane.setConstraints(textField, 2, i +1);
+            amountField[i] = textField;
             grid.getChildren().add(textField);
         }
 
         // how to access
-        // textFields[5].setPromptText("item5");
-        // textFields[1].setText("testi");
-        // System.out.println(textFields[1].getText());
+        // itemField[5].setPromptText("item5");
+        // itemField[1].setText("testi");
+        // System.out.println(itemField[1].getText());
 
         Button addAll = new Button("Add all");
         GridPane.setConstraints(addAll, 0, 11);
@@ -59,7 +73,10 @@ public class Main extends Application {
         Button removeAll = new Button("Remove all");
         GridPane.setConstraints(removeAll, 1, 11);
 
-        grid.getChildren().addAll(shoppingList, addAll, removeAll);
+        Button saveToFile = new Button("Save to file");
+        GridPane.setConstraints(saveToFile, 2, 11);
+
+        grid.getChildren().addAll(shoppingList, addAll, removeAll, saveToFile);
 
         Scene scene = new Scene(grid, 800, 600);
         stage.setScene(scene);
